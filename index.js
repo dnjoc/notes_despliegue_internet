cf72585161ff3aeb720af5b03b01c8c85d4984b3
Dnjoc <dnjoc.dc@gmail.com>
2024-10-07 15:32:22 -0400

    Configuracion de errores y del put y delete con mongodb

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const express = require('express')
 const app = express()
 
 require('dotenv').config()
-
 const Note = require('./models/note')
-
-const cors = require('cors')
-
-app.use(cors())
 //agregamos el middleware integrado de Express llamado static
 app.use(express.static('dist'))
-
 let notes = [
   {
     id: 1,
@@ -37,8 +31,25 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+const cors = require('cors')
+
+app.use(cors())
 app.use(express.json())
 app.use(requestLogger)
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 //Configuracion de mongodb atlas
 
 //const password = process.argv[2]
@@ -47,11 +58,6 @@ app.use(requestLogger)
 
 //const Note = mongoose.model('Note', noteSchema)
 //const Note = require('./models/note')
-
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
 // const generateId = () => {
 //     const maxId = notes.length > 0
 //       ? Math.max(...notes.map(n => n.id))
@@ -84,11 +90,22 @@ app.get('/', (request, response) => {
   // })
 
   //usando el metodo findById de mongoose
-  app.get('/api/notes/:id', (request, response) => {
-    console.log(request.body.id)
-    Note.findById(request.params.id).then(note => {
-      response.json(note)
+  app.get('/api/notes/:id', (request, response, next) => {
+    console.log(request.params.id)
+    Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
     })
+    .catch(error => next(error))
+    // .catch(error => {
+    //   console.log(error)
+    //   //response.status(500).end()
+    //   response.status(400).send({ error: 'malformatted id' })
+    //})
   })
 
   // app.post('/api/notes', (request, response) => {
@@ -129,15 +146,39 @@ app.get('/', (request, response) => {
     })
   })
 
-  app.delete('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    notes = notes.filter(note => note.id !== id)
+  //configuracion para actualizar la importancia de una nota en mongodb
+  app.put('/api/notes/:id', (request, response, next) => {
+    const body = request.body
   
-    response.status(204).end()
+    const note = {
+      content: body.content,
+      important: body.important,
+    }
+  
+    Note.findByIdAndUpdate(request.params.id, note, { new: true })
+      .then(updatedNote => {
+        response.json(updatedNote)
+      })
+      .catch(error => next(error))
   })
 
-  app.use(unknownEndpoint)
+  // app.delete('/api/notes/:id', (request, response) => {
+  //   const id = Number(request.params.id)
+  //   notes = notes.filter(note => note.id !== id)
   
+  //   response.status(204).end()
+  // })
+  //Configuracion para eliminar una nota en mongoDB
+  app.delete('/api/notes/:id', (request, response, next) => {
+    Note.findByIdAndDelete(request.params.id)
+      .then(result => {
+        response.status(204).end()
+      })
+      .catch(error => next(error))
+  })
+
+  app.use(unknownEndpoint) 
+  app.use(errorHandler)
   //const PORT = process.env.PORT || 3001
   const PORT = process.env.PORT
   app.listen(PORT, () => {
