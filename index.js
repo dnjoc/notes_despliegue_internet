e951e9e74c3d5a8fc397722950d9f95bcf4c8d22
Dnjoc <dnjoc.dc@gmail.com>
2024-10-14 21:59:38 -0400

    Configurando eslint y solventando errores

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ require('dotenv').config()
 const Note = require('./models/note')
 //agregamos el middleware integrado de Express llamado static
 app.use(express.static('dist'))
-let notes = [
-  {
-    id: 1,
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: 2,
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: 3,
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
-]
+// let notes = [
+//   {
+//     id: 1,
+//     content: "HTML is easy",
+//     important: true
+//   },
+//   {
+//     id: 2,
+//     content: "Browser can execute only JavaScript",
+//     important: false
+//   },
+//   {
+//     id: 3,
+//     content: "GET and POST are the most important methods of HTTP protocol",
+//     important: true
+//   }
+// ]
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -38,8 +38,8 @@ const errorHandler = (error, request, response, next) => {
     return response.status(400).send({ error: 'malformatted id' })
   }
   else if (error.name === 'ValidationError') {
-     return response.status(400).json({ error: error.message })
- }
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
@@ -68,34 +68,34 @@ const unknownEndpoint = (request, response) => {
 //     return maxId + 1
 //   }
 app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  response.send('<h1>Hello World!</h1>')
+})
+
+//configuracion get para consulta en mongodb
+app.get('/api/notes', (request, response) => {
+  Note.find({}).then(notes => {
+    response.json(notes)
   })
+})
+// app.get('/api/notes', (request, response) => {
+//   response.json(notes)
+// })
 
- //configuracion get para consulta en mongodb
-  app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
-  })
-  // app.get('/api/notes', (request, response) => {
-  //   response.json(notes)
-  // })
+// app.get('/api/notes/:id', (request, response) => {
 
-  // app.get('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   const note = notes.find(note => note.id === id)
+//   if (note) {
+//       response.json(note)
+//     } else {
+//       response.status(404).end()
+//     }
+// })
 
-  //   const id = Number(request.params.id)
-  //   const note = notes.find(note => note.id === id)
-  //   if (note) {
-  //       response.json(note)
-  //     } else {
-  //       response.status(404).end()
-  //     }
-  // })
-
-  //usando el metodo findById de mongoose
-  app.get('/api/notes/:id', (request, response, next) => {
-    console.log(request.params.id)
-    Note.findById(request.params.id)
+//usando el metodo findById de mongoose
+app.get('/api/notes/:id', (request, response, next) => {
+  console.log(request.params.id)
+  Note.findById(request.params.id)
     .then(note => {
       if (note) {
         response.json(note)
@@ -104,93 +104,94 @@ app.get('/', (request, response) => {
       }
     })
     .catch(error => next(error))
-    // .catch(error => {
-    //   console.log(error)
-    //   //response.status(500).end()
-    //   response.status(400).send({ error: 'malformatted id' })
-    //})
-  })
+  // .catch(error => {
+  //   console.log(error)
+  //   //response.status(500).end()
+  //   response.status(400).send({ error: 'malformatted id' })
+  //})
+})
 
-  // app.post('/api/notes', (request, response) => {
-  //   const body = request.body
+// app.post('/api/notes', (request, response) => {
+//   const body = request.body
 
-  //   if (!body.content) {
-  //     return response.status(400).json({
-  //       error: 'content missing'
-  //     })
-  //   }
+//   if (!body.content) {
+//     return response.status(400).json({
+//       error: 'content missing'
+//     })
+//   }
 
-  //   const note = {
-  //     content: body.content,
-  //     important: Boolean(body.important) || false,
-  //     id: generateId(),
-  //   }
+//   const note = {
+//     content: body.content,
+//     important: Boolean(body.important) || false,
+//     id: generateId(),
+//   }
 
-  //   notes = notes.concat(note)
+//   notes = notes.concat(note)
 
-  //   response.json(note)
-  // })
+//   response.json(note)
+// })
 
 //Configuracion para crear una nueva nota en mogodb
-  app.post('/api/notes', (request, response, next) => {
-    const body = request.body
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
-    if (body.content === undefined) {
-      return response.status(400).json({ error: 'content missing' })
-    }
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    const note = new Note({
-      content: body.content,
-      important: body.important || false,
-    })
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    note.save()
+  note.save()
     .then(savedNote => {
       response.json(savedNote)
     })
     .catch(error => next(error))
-  })
+})
 
-  //configuracion para actualizar la importancia de una nota en mongodb
-  app.put('/api/notes/:id', (request, response, next) => {
-    // const body = request.body
-    const { content, important } = request.body
-    // const note = {
-    //   content: body.content,
-    //   important: body.important,
-    // }
+//configuracion para actualizar la importancia de una nota en mongodb
+app.put('/api/notes/:id', (request, response, next) => {
+  // const body = request.body
+  const { content, important } = request.body
+  // const note = {
+  //   content: body.content,
+  //   important: body.important,
+  // }
 
-    // Note.findByIdAndUpdate(request.params.id, note, { new: true })
-    Note.findByIdAndUpdate(
-      request.params.id, 
-      { content, important },
-      { new: true, runValidators: true, context: 'query' }
-    ) 
-      .then(updatedNote => {
-        response.json(updatedNote)
-      })
-      .catch(error => next(error))
-  })
+  // Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id,
+    { content, important },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
 
-  // app.delete('/api/notes/:id', (request, response) => {
-  //   const id = Number(request.params.id)
-  //   notes = notes.filter(note => note.id !== id)
+// app.delete('/api/notes/:id', (request, response) => {
+//   const id = Number(request.params.id)
+//   notes = notes.filter(note => note.id !== id)
 
-  //   response.status(204).end()
-  // })
-  //Configuracion para eliminar una nota en mongoDB
-  app.delete('/api/notes/:id', (request, response, next) => {
-    Note.findByIdAndDelete(request.params.id)
-      .then(result => {
-        response.status(204).end()
-      })
-      .catch(error => next(error))
-  })
+//   response.status(204).end()
+// })
+//Configuracion para eliminar una nota en mongoDB
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      console.log(result)
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-  app.use(unknownEndpoint)
-  app.use(errorHandler)
-  //const PORT = process.env.PORT || 3001
-  const PORT = process.env.PORT
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.use(unknownEndpoint)
+app.use(errorHandler)
+//const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
